f45e055fb0ef250e148f24ac5e626b2e00450e7b
Cynthia Teeters <glean@cynthiateeters.com>
2021-03-19 17:29:28 -0400

    Add code

---
+++ resistor.js
@@ -0,0 +1,14 @@
+/* resistor.js */
+
+/*
+  elctronic resistors have colored bands where each color represents a numerical number
+  Electrical engineers can read the colors and
+  determine the resitance value in Ohms
+
+  see this resistor calculator for help
+   http://bit.ly/2NjS274
+*/
+
+const getResistorOhms = (bands) => {};
+
+export { getResistorOhms };
